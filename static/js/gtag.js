451298f2c505bb08ec7e15af4deba6bd1f16a709
M. Akif Tokatlioglu <akifmt@gmail.com>
2024-04-26 23:20:07 +0300

    added gtag

---
+++ static/js/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-S9QF79W9GM"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-S9QF79W9GM');
+</script>
